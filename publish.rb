b38542b9f0846f615fe54657df3f7d4f7b3e8c87
happy <happy@example.com>
2023-01-31 02:53:27 -0800

    add

---
+++ publish.rb
@@ -0,0 +1,30 @@
+
+version = ARGV[0]
+puts "Version:#{version}"
+
+unless version =~ /^\d+\.\d\.\d(\-[a-z.0-9]+)?$/
+  STDERR.puts "Expected version as argument instead got: #{version}"
+  exit 1
+end
+
+path = File.join(File.dirname(__FILE__), 
+  if version.split('.')[0] == "5"
+    '@react-native-mapbox-gl-mapbox-static.5.template.podspec.json'
+  else
+    '@react-native-mapbox-gl-mapbox-static.template.podspec.json'
+  end
+)
+
+lines = []
+File.readlines(path).each do |line|
+  line = line.gsub(/\_\.\_\.\_/,version)
+  lines.push(line)
+end
+
+File.write(File.join(File.dirname(__FILE__), "@react-native-mapbox-gl-mapbox-static.podspec.json"), lines.join(""))
+
+if ARGV[1] == "autopublish"
+  system("pod trunk push \"@react-native-mapbox-gl-mapbox-static.podspec.json\" --skip-import-validation --allow-warnings")
+else
+  puts "Please execute: `pod trunk push \"@react-native-mapbox-gl-mapbox-static.podspec.json\" --skip-import-validation --allow-warnings`"
+end
